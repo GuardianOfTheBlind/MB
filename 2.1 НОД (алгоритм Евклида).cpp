e384942ea007199601e81c30929e2148fc80fb9f
GuardianOfTheBlind <53496843+GuardianOfTheBlind@users.noreply.github.com>
2019-11-10 20:07:36 +0300

    Update 2.1 НОД (алгоритм Евклида).cpp

--- 2.1 НОД (алгоритм Евклида).cpp
+++ 2.1 НОД (алгоритм Евклида).cpp
@@ -1,1 +1,33 @@
+// Наибольший общий делитель
+// цикл
+
+#include <iostream>
+
+
+unsigned gcd(unsigned a, unsigned b);
+
+int main()
+{
+	unsigned value1, value2, result;
+
+	std::cout << "Input two values: ";
+	std::cin >> value1 >> value2;
+
+	result = gcd(value1, value2);
+
+	std::cout << "GCD(" << value1 << ", " << value2 << ") = " << result << std::endl;
+	return 0;
+}
+
+unsigned gcd(unsigned a, unsigned b) {
+	unsigned c;
+
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
 
