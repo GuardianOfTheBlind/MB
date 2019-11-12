e8ea29e167433db2f8266ae21d75f1ee16385404
GuardianOfTheBlind <53496843+GuardianOfTheBlind@users.noreply.github.com>
2019-11-12 05:49:57 +0300

    Update 2.1 НОД (алгоритм Евклида - рекурсия).cpp

--- 2.1 НОД (алгоритм Евклида - рекурсия).cpp
+++ 2.1 НОД (алгоритм Евклида - рекурсия).cpp
@@ -1,1 +1,27 @@
+// Наибольший общий делитель
+// рекурсия
 
+#include <iostream>
+
+
+unsigned gcd(const unsigned a, const unsigned b);
+
+int main()
+{
+	unsigned value1, value2, result;
+
+	std::cout << "Input two values: ";
+	std::cin >> value1 >> value2;
+
+	result = gcd(value1, value2);
+
+	std::cout << "GCD(" << value1 << ", " << value2 << ") = " << result << std::endl;
+	return 0;
+}
+
+unsigned gcd(const unsigned a, const unsigned b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
